2ee3d4a5805c4aa50b87c37c51e956e494180898
Kinjal Patel <patelkinjal298@hotmail.com>
2016-06-02 16:33:13 -0400

    Add project files.

---
+++ COMP2007-Lesson4-LAB3/Courses.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace COMP2007_Lesson4_LAB3
+{
+    public partial class Courses : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ COMP2007-Lesson4-LAB3/Departments.aspx
@@ -0,0 +1,4 @@
+﻿<%@ Page Title="Departments" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Departments.aspx.cs" Inherits="COMP2007_Lesson4_LAB3.Departments" %>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+</asp:Content>
